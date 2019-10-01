ec7338ed41536171ecaf6d007f3f4da86ed2eaee
SschoolFighter <galihcol1@gmail.com>
2019-10-02 03:02:43 +0700

    duarr

---
+++ audio.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form name="audio_form" id="audio_form" action="upload.php" method="post" enctype="multipart/form-data">
+<fieldset>
+<label>Audio File:</label>
+<input name="audio_file" id="audio_file" type="file"/>
+<input type="submit" name="Submit" id="Submit" value="Submit"/>
+</fieldset>
+</form>
+</body>
+</html>
